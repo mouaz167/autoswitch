80d36a81877d99f70a4a80a8d0b17f4dc0f662bd
mouaz167 <59470537+mouaz167@users.noreply.github.com>
2025-08-12 23:39:37 +1000

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,35 +1,26 @@
-// ==UserScript==
+// ==UserPlugin==
 // @name         Auto Switch to New Channel in Guild
-// @version      1.0
-// @description  Switches to a newly created channel instantly in a specified guild
 // @author       ChatGPT
-// ==/UserScript==
+// @description  Switches to a newly created channel instantly in a specified guild
+// @version      1.0
+// ==/UserPlugin==
 
 const targetGuildId = "1274584500743180319";
 
-const { React, channels, transitionToChannel } = vendored;
+const { FluxDispatcher } = require("@webpack").findByProps("dispatch", "_currentDispatchActionType");
+const { transitionToChannel } = require("@webpack").findByProps("transitionTo", "replaceWith");
 
-function onChannelCreate(channel) {
-  // Check if the channel is in the target guild
-  if (channel.guild_id === targetGuildId) {1274584500743180319}
-    // Switch to the new channel instantly
-    transitionToChannel(channel.id);
-  }
-}
-
-function start() {
-  // Patch the channel creation event emitter
-  // Discord uses a dispatcher for channel events
-
-  // vendored.events is an event emitter we can hook into
-  vendored.events.on("CHANNEL_CREATE", onChannelCreate);
-}
-
-function stop() {
-  vendored.events.off("CHANNEL_CREATE", onChannelCreate);
+function onChannelCreate({ channel }) {
+    if (channel?.guild_id === targetGuildId) {
+        transitionToChannel(channel.id);
+    }
 }
 
 module.exports = {
-  start,
-  stop,
+    start() {
+        FluxDispatcher.subscribe("CHANNEL_CREATE", onChannelCreate);
+    },
+    stop() {
+        FluxDispatcher.unsubscribe("CHANNEL_CREATE", onChannelCreate);
+    }
 };
